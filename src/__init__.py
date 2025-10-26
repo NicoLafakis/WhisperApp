702c1e93b593f0c99a7b03ccb8ba62bef2df8e41
Nick L <lafakisn@gmail.com>
2025-10-26 18:56:36 -0400

    Scorched Earth - Completely removed all previous files from the repository in order to start over fresh/new.

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-WhisperApp - Push-to-Talk Transcription Application
-"""
-
-__version__ = "1.0.0"
-__author__ = "WhisperApp Team"
-__description__ = "Push-to-Talk transcription using OpenAI Whisper"
